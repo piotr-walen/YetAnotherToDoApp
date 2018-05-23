346c0da477fa6049778d45adb6d116a5266c280c
Piotr Walen <walen.piotr@gmail.com>
2018-05-23 14:29:26 +0200

    Add update route Refactor create and read routes Remove comments

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -3,22 +3,18 @@ const router = express.Router();
 const pool = require('../database');
 
 router.post('/api/v1/todos', async (request, response, next) => {
-    const results = [];
-    // Grab data from http request
-    const data = { text: request.body.text, complete: false };
+    const { text } = request.body;
+    const complete = false;
     try {
-        // Get a Postgres client from the connection pool
         client = await pool.connect();
-        // SQL Query > Insert Data
         await client.query('INSERT INTO items(text, complete) values($1, $2)', [
-            data.text,
-            data.complete
+            text,
+            complete
         ]);
-        // SQL Query > Select Data
-        const databaseResponse = await client.query(
+        const dbData = await client.query(
             'SELECT * FROM items ORDER BY id ASC'
         );
-        response.status(200).json({ data: results });
+        response.status(200).json({ data: dbData.rows });
         client.release();
     } catch (error) {
         next(error);
@@ -26,16 +22,31 @@ router.post('/api/v1/todos', async (request, response, next) => {
 });
 
 router.get('/api/v1/todos', async (request, response, next) => {
-    const results = [];
     try {
-        // Get a Postgres client from the connection pool
         client = await pool.connect();
-        // SQL Query > Select Data
-        const databaseResponse = await client.query(
+        const dbData = await client.query(
             'SELECT * FROM items ORDER BY id ASC'
         );
-        response.status(200).json({ data: databaseResponse.rows });
+        response.status(200).json({ data: dbData.rows });
         client.release();
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.put('/api/v1/todos/:todo_id', async (request, response, next) => {
+    const { text, complete } = request.body;
+    const { todo_id } = request.params;
+    try {
+        client = await pool.connect();
+        await client.query(
+            'UPDATE items SET text=($1), complete=($2) WHERE id=($3)',
+            [text, complete, todo_id]
+        );
+        const dbData = await client.query(
+            'SELECT * FROM items ORDER BY id ASC'
+        );
+        response.status(200).json({ data: dbData.rows });
     } catch (error) {
         next(error);
     }
