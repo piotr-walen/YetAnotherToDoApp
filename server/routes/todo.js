811e36d9d8221dcbb3988123fa96e5cfdeeb0c39
Piotr Walen <walen.piotr@gmail.com>
2018-05-23 13:31:54 +0200

    Add reference to pool

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL;
+const pool = require('../database');
 
 router.post('/api/v1/todos', async (request, response, next) => {
     const results = [];
     // Grab data from http request
     const data = { text: request.body.text, complete: false };
-    // Get a Postgres client from the connection pool
-    const pool = new pg.Pool({ connectionString });
-    pool.on('error', (error, client) => {
-        next(error);
-        process.exit(-1);
-    });
-
     try {
+        // Get a Postgres client from the connection pool
         client = await pool.connect();
         // SQL Query > Insert Data
         await client.query('INSERT INTO items(text, complete) values($1, $2)', [
@@ -31,7 +24,29 @@ router.post('/api/v1/todos', async (request, response, next) => {
             results.push(row);
         });
         // After all data is returned, close connection and return results
-        client.end();
+        client.release();
+        response.status(200).json({ data: results });
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/api/v1/todos', async (request, response, next) => {
+    const results = [];
+    try {
+        // Get a Postgres client from the connection pool
+        client = await pool.connect();
+        // SQL Query > Select Data
+        const databaseResponse = await client.query(
+            'SELECT * FROM items ORDER BY id ASC'
+        );
+        // Stream results back one row at a time
+        databaseResponse.rows.forEach(row => {
+            console.log(row);
+            results.push(row);
+        });
+        // After all data is returned, close connection and return results
+        client.release();
         response.status(200).json({ data: results });
     } catch (error) {
         next(error);
