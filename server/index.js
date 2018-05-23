f44c3d8fb30cf89fb7d8c02e1723968ae678b5ab
Piotr Walen <walen.piotr@gmail.com>
2018-05-23 15:28:56 +0200

    Add not found route

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const todo = require('./routes/todo');
 app.use(todo);
 
+app.use(function(request, response, next) {
+    let error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
 app.use(function(error, request, response, next) {
     console.log(error);
     response.status(error.status || 500).send({ error: error.message });
