c753df9b6f9d49c0404dd413eede88a685f89782
Piotr Walen <walen.piotr@gmail.com>
2018-05-23 16:07:21 +0200

    Remove export

--- server/models/todos.js
+++ server/models/todos.js
@@ -1,8 +1,9 @@
+require('dotenv').config({ path: '../.env' });
 const pg = require('pg');
 const connectionString = process.env.DATABASE_URL;
 const client = new pg.Client(connectionString);
 
-async function createTable() {
+async function create() {
     await client.connect();
     var result = await client.query(
         'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)'
@@ -10,5 +11,3 @@ async function createTable() {
     console.log(result.rows);
     client.end();
 }
-
-module.exports = createTable;
