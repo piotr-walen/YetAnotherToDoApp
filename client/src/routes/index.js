2ede719fb2f9a2c75486f9c668856d84f7bdfbcb
Piotr Walen <walen.piotr@gmail.com>
2018-05-24 09:53:23 +0200

    Add index router

---
+++ client/src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch, withRouter } from 'react-router';
+
+const Routes = () => (
+    <Switch>
+        <Route exact path="/" component={() => <div>Home Page</div>} />
+        <Route path="/login" component={() => <div>Login Form</div>} />
+        <Route path="/logout" component={() => <div>Logout</div>} />
+        <Route path="/register" component={() => <div>Login Form</div>} />
+        <Route component={() => <NotFound />} />
+    </Switch>
+);
+
+export default withRouter(Routes);
