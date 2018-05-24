02e901a8f9993095936ef7f1b26bff3a72c32dcc
Piotr Walen <walen.piotr@gmail.com>
2018-05-24 15:14:12 +0200

    Add LogoutView to logout route Fix import RegisterForm

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Switch, withRouter } from 'react-router';
 import LoginForm from '../containers/LoginForm';
-import RegisterForm from '../containers/Register';
+import RegisterForm from '../containers/RegisterForm';
+import LogoutView from '../containers/LogoutView';
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={() => <div>Home Page</div>} />
         <Route path="/login" component={props => <LoginForm {...props} />} />
-        <Route path="/logout" component={() => <div>Logout</div>} />
+        <Route path="/logout" component={props => <LogoutView {...props} />} />
         <Route
             path="/register"
             component={props => <RegisterForm {...props} />}
