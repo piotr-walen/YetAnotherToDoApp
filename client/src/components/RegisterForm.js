e402565b078b0b23fcf2a1286c03b1066cee6b26
Piotr Walen <walen.piotr@gmail.com>
2018-05-24 23:07:18 +0200

    Add styles to form

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -48,3 +48,48 @@ export default class Register extends Component {
         );
     }
 }
+
+const Label = styled.label`
+    font-size: 24px;
+    margin-top: 10px;
+    margin: 10px auto 0px;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    border: 1px solid grey;
+    border-radius: 4px;
+    margin: 15px auto 0px;
+`;
+
+const Input = styled.input`
+    border: none;
+    background-image: none;
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    height: 40px;
+    margin: 10px 10px 0px;
+    border-radius: 4px;
+    text-indent: 8px;
+    font-size: 20px;
+    border: 1px solid grey;
+`;
+
+const Button = styled.button`
+    border: none;
+    background-image: none;
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    font-size: 20px;
+    height: 40px;
+    margin: 10px 10px 10px;
+    border-radius: 4px;
+    background-color: rgba(10, 50, 200, 1);
+    color: white;
+`;
