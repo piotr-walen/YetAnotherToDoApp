4111eafe62e2174f88f86dbd4edd951e478c4486
Piotr Walen <walen.piotr@gmail.com>
2018-05-24 14:56:32 +0200

    Add RegisterForm container

---
+++ client/src/containers/RegisterForm.js
@@ -0,0 +1,42 @@
+import RegisterForm from '../components/RegisterForm';
+import * as auth from '../actions/auth';
+import { connect } from 'react-redux';
+import React from 'react';
+
+let createHandlers = ({ history, dispatch }) => {
+    let register = async function(data) {
+        const request = {
+            type: 'cors',
+            body: JSON.stringify({
+                ...data
+            }),
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        };
+        try {
+            const response = await fetch('/api/auth/register/', request);
+            const parsed = await response.json();
+            console.log(parsed);
+            if (parsed.error) {
+                throw new Error(parsed.error.message);
+            }
+            if (parsed.data) {
+                dispatch(auth.login(parsed.data));
+                history.push('/');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return {
+        register
+    };
+};
+
+export default connect()(props => (
+    <RegisterForm handlers={createHandlers(props)} />
+));
