2d68c05dd777d12aeec5dce7da4e4b40980f2c70
Piotr Walen <walen.piotr@gmail.com>
2018-05-24 23:07:01 +0200

    Add styles to form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import styled from 'styled-components';
 export default class LoginForm extends Component {
     state = {
         username: '',
@@ -29,22 +29,67 @@ export default class LoginForm extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label> Login </label>
-                <input
+            <Form onSubmit={this.handleSubmit}>
+                <Label> Login </Label>
+                <Input
                     type="text"
                     value={this.state.username}
                     onChange={this.handleChange('username')}
                     placeholder="username"
                 />
-                <input
+                <Input
                     value={this.state.password}
                     type="password"
                     onChange={this.handleChange('password')}
                     placeholder="password"
                 />
-                <button type="submit">Submit</button>
-            </form>
+                <Button type="submit">Submit</Button>
+            </Form>
         );
     }
 }
+
+const Label = styled.label`
+    font-size: 24px;
+    margin-top: 10px;
+    margin: 10px auto 0px;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    border: 1px solid grey;
+    border-radius: 4px;
+    margin: 15px auto 0px;
+`;
+
+const Input = styled.input`
+    border: none;
+    background-image: none;
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    height: 40px;
+    margin: 10px 10px 0px;
+    border-radius: 4px;
+    text-indent: 8px;
+    font-size: 20px;
+    border: 1px solid grey;
+`;
+
+const Button = styled.button`
+    border: none;
+    background-image: none;
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    font-size: 20px;
+    height: 40px;
+    margin: 10px 10px 10px;
+    border-radius: 4px;
+    background-color: rgba(10, 50, 200, 1);
+    color: white;
+`;
