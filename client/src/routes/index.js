e79078d8f5bd89664752cabc0f379ac7f30e41a6
Piotr Walen <walen.piotr@gmail.com>
2018-05-24 14:58:07 +0200

    Add RegisterForm to router

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Route, Switch, withRouter } from 'react-router';
 import LoginForm from '../containers/LoginForm';
+import RegisterForm from '../containers/Register';
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={() => <div>Home Page</div>} />
         <Route path="/login" component={props => <LoginForm {...props} />} />
         <Route path="/logout" component={() => <div>Logout</div>} />
-        <Route path="/register" component={() => <div>Login Form</div>} />
+        <Route
+            path="/register"
+            component={props => <RegisterForm {...props} />}
+        />
         <Route component={() => <div>Not Found</div>} />
     </Switch>
 );
