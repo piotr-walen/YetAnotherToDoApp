a4df91f020b79f0a71bdabe3cb3ac84a0f37a7cb
Piotr Walen <walen.piotr@gmail.com>
2018-05-24 11:53:29 +0200

    Add props to LoginForm route

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Switch, withRouter } from 'react-router';
+import LoginForm from '../containers/LoginForm';
 
 const Routes = () => (
     <Switch>
         <Route exact path="/" component={() => <div>Home Page</div>} />
-        <Route path="/login" component={() => <div>Login Form</div>} />
+        <Route path="/login" component={props => <LoginForm {...props} />} />
         <Route path="/logout" component={() => <div>Logout</div>} />
         <Route path="/register" component={() => <div>Login Form</div>} />
         <Route component={() => <div>Not Found</div>} />
