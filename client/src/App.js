efd1a2858a933802a60d2e0af5f9d46a5f974828
Piotr Walen <walen.piotr@gmail.com>
2018-05-24 10:45:06 +0200

    Move Links to Navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,14 @@ import React, { Component } from 'react';
 import Routes from './routes';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
-import { Link } from 'react-router-dom';
 
+import Navbar from './containers/Navbar';
 class App extends Component {
     render() {
         return (
             <div>
                 <div>Yet Another ToDo List</div>
-                
-                <div>
-                    <Link to="/">link to /</Link>
-                </div>
-                <div>
-                    <Link to="/login">link to /login/</Link>
-                </div>
-                <div>
-                    <Link to="/register">link to /register/</Link>
-                </div>
-                <div>
-                    <Link to="/logout">link to /logout/</Link>
-                </div>
-
+                <Navbar />
                 <Routes />
             </div>
         );
