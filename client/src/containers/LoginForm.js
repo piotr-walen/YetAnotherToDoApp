0af8fa2347a1d6019b86552d8034e6c3b787459f
Piotr Walen <walen.piotr@gmail.com>
2018-05-24 11:50:25 +0200

    Add LoginForm container

---
+++ client/src/containers/LoginForm.js
@@ -0,0 +1,42 @@
+import LoginForm from '../components/LoginForm';
+import * as auth from '../actions/auth';
+import { connect } from 'react-redux';
+import React from 'react';
+
+let createHandlers = ({ history, dispatch }) => {
+    let login = async function(data) {
+        const request = {
+            type: 'cors',
+            body: JSON.stringify({
+                ...data
+            }),
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        };
+        try {
+            const response = await fetch('/api/auth/login/', request);
+            const parsed = await response.json();
+            console.log(parsed);
+            if (parsed.error) {
+                throw new Error(parsed.error.message);
+            }
+            if (parsed.data) {
+                dispatch(auth.login(parsed.data));
+                history.push('/');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return {
+        login
+    };
+};
+
+export default connect()(props => (
+    <LoginForm handlers={createHandlers(props)} />
+));
