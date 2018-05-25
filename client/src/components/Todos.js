b0f9082ed7d35995b048c588b254b857b3184237
Piotr Walen <walen.piotr@gmail.com>
2018-05-25 15:38:30 +0200

    Add styles to todos Add CheckedTodo component Add Todo component

--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+
 class Todos extends React.Component {
     componentDidMount() {
         this.props.loadTodos(this.props.user);
     }
     render() {
         return (
-            <div>
-                {this.props.todos.map(todo => (
-                    <li key={todo.id}>{todo.text}</li>
-                ))}
-            </div>
+            <List>
+                {this.props.todos.map(
+                    todo =>
+                        todo.complete ? (
+                            <CheckedTodo key={todo.id}>{todo.text}</CheckedTodo>
+                        ) : (
+                            <Todo key={todo.id}>{todo.text}</Todo>
+                        )
+                )}
+            </List>
         );
     }
 }
+
 export default Todos;
+
+const List = styled.div`
+    margin: 50px auto;
+    width: 500px;
+`;
+
+const Todo = styled.li`
+    font-size: 24px;
+`;
+
+const CheckedTodo = styled(Todo)`
+    text-decoration: line-through;
+`;
