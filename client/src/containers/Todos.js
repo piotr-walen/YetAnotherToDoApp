bb146ada98257bc36ff9f80a62a28d288d7794a1
Piotr Walen <walen.piotr@gmail.com>
2018-05-25 11:17:30 +0200

    Add state to Todos container

--- client/src/containers/Todos.js
+++ client/src/containers/Todos.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import * as todos from '../actions/todos';
+import { connect } from 'react-redux';
 
-const Todos = () => <div>todos...</div>;
+const mapDispatchToProps = dispatch => ({
+    loadTodos: userId => {
+        dispatch(todos.loadTodos(todos));
+    }
+});
 
-export default Todos;
+const mapStateToProps = state => ({
+    authenticated: state.auth.authenticated,
+    user: state.auth.user,
+    todos: state.todos.todos
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(({ todos }) => (
+    <div>{todos.map(todo => <li>{todo.text}</li>)}</div>
+));
