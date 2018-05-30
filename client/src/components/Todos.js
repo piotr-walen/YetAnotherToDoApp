d38228431ba98000c04e3694174ef64b90ff36a2
Piotr Walen <walen.piotr@gmail.com>
2018-05-30 11:30:10 +0200

    Add dividers for todos Add delete icon svg

--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -57,7 +57,7 @@ class Todos extends React.Component {
                     {this.props.todos.map(todo => (
                         <TodoGroup key={todo.id}>
                             <DeleteIcon onClick={this.handleDelete(todo)}>
-                                {'X '}
+                                <path d="M 10.3125 -0.03125 C 8.589844 -0.03125 7.164063 1.316406 7 3 L 2 3 L 2 5 L 6.96875 5 L 6.96875 5.03125 L 17.03125 5.03125 L 17.03125 5 L 22 5 L 22 3 L 17 3 C 16.84375 1.316406 15.484375 -0.03125 13.8125 -0.03125 Z M 10.3125 2.03125 L 13.8125 2.03125 C 14.320313 2.03125 14.695313 2.429688 14.84375 2.96875 L 9.15625 2.96875 C 9.296875 2.429688 9.6875 2.03125 10.3125 2.03125 Z M 4 6 L 4 22.5 C 4 23.300781 4.699219 24 5.5 24 L 18.59375 24 C 19.394531 24 20.09375 23.300781 20.09375 22.5 L 20.09375 6 Z M 7 9 L 8 9 L 8 22 L 7 22 Z M 10 9 L 11 9 L 11 22 L 10 22 Z M 13 9 L 14 9 L 14 22 L 13 22 Z M 16 9 L 17 9 L 17 22 L 16 22 Z " />
                             </DeleteIcon>
                             <Todo
                                 onClick={this.handleClick(todo)}
@@ -135,16 +135,25 @@ const Todo = styled.span`
     -moz-user-select: none;
     -ms-user-select: none;
     user-select: none;
+    margin-left: 4px;
     text-decoration: ${({ complete }) => (complete ? 'line-through' : 'none')};
 `;
 
 const TodoGroup = styled.li`
+    display: flex;
+    align-items: center;
     padding: 0;
+    border-bottom: 1px solid grey;
+    :first-child {
+        border-top: 1px solid grey;
+    }
 `;
 
-const DeleteIcon = styled.label`
+const DeleteIcon = styled.svg`
+    width: 24px;
+    height: 24px;
     opacity: 0;
-    color: red;
+    fill: rgba(250, 50, 50, 0.9);
     transition: opacity 0.3s ease-in-out;
     ${TodoGroup}:hover & {
         opacity: 1;
