276e3b897c5090017e4ac5700be9810a2746bb6f
Piotr Walen <walen.piotr@gmail.com>
2018-06-11 17:18:31 +0200

    Add HomePage component

---
+++ client/src/components/HomePage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const HomePage = () => <Box>Home Page</Box>;
+
+const Box = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 10px;
+`;
+
+export default HomePage;
