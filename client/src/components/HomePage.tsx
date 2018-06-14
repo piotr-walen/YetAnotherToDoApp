713468668956234c41e490fc7a6409b7ce7134f5
Piotr Walen <walen.piotr@gmail.com>
2018-06-14 20:13:40 +0200

    Modify home page

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-const HomePage = () => <Box><h1>Home</h1></Box>;
+const HomePage = () => (
+    <Box>
+        <h1>Yet Another To-Do App</h1>
+        <h2>Created with TypeScript</h2>
+        <h3>
+            Client technologies: react, redux, react-router, styled-components
+        </h3>
+        <h3>Server technologies: nodeJS, express, postgreSQL </h3>
+        <h3>Authentication using: jsonwebtoken, bcrypt </h3>
+        <h3>Routes testing using: mocha, chai, supertest</h3>
+    </Box>
+);
 
 const Box = styled.div`
-    display: flex;
-    justify-content: center;
-    padding: 10px;
+    padding-left: 200px;
+    padding-right: 200px;
 `;
 
 export default HomePage;
