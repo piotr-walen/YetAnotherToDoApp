f3fcf1095afc70d29058b7f2103789ab602393bf
Piotr Walen <walen.piotr@gmail.com>
2018-06-14 10:42:17 +0200

    Add put route tests

--- server/src/api_tests/todos.test.ts
+++ server/src/api_tests/todos.test.ts
@@ -72,3 +72,52 @@ describe('GET /api/user/:userId/todos', async () => {
         await todos.deleteUsersTodos(data.userid);
     });
 });
+
+describe('PUT /api/user/:userId/todos/:id', async () => {
+    let dbtodos: any[];
+    let user: { username: string; id: number; token: string };
+    let newTodo: any;
+    let updateTodo: any;
+    before(async () => {
+        const username = 'test_username1';
+        const password = 'test_password';
+        user = await auth.createUser(username, password);
+        newTodo = { userid: user.id, text: 'test_text', complete: false };
+        dbtodos = await todos.createTodo(
+            user.id,
+            newTodo.text,
+            newTodo.complete,
+        );
+        updateTodo = {
+            userid: user.id,
+            text: 'updated_test_text',
+            complete: true,
+        };
+    });
+    it('it should return user todos', done => {
+        request(app)
+            .put(`/api/user/${user.id}/todos/${dbtodos[0].id}`)
+            .set('Accept', 'application/json')
+            .set('Authorization', `Bearer: ${user.token}`)
+            .set('Content-Type', 'application/json')
+            .send(updateTodo)
+            .end((error, response: any) => {
+                if (error) return done(error);
+                expect(response.statusCode).to.equal(200);
+                expect(response.body.data).to.be.an('array');
+                expect(response.body.data.length).to.equal(1);
+                expect(response.body.data[0].text).to.be.equal(updateTodo.text);
+                expect(response.body.data[0].userid).to.be.equal(
+                    updateTodo.userid,
+                );
+                expect(response.body.data[0].complete).to.be.equal(
+                    updateTodo.complete,
+                );
+                done();
+            });
+    });
+    after(async () => {
+        await auth.removeUser(user.username);
+        await todos.deleteUsersTodos(user.id);
+    });
+});
